b98989bef5db071123966232f1363cf2c57f46ee
Nicolas <nicolas.belan@gmail.com>
2017-04-09 18:07:17 +0200

    full puppet server instance

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,19 @@
 FROM centos:7
 MAINTAINER nicolas.belan@gmail.com
 
-VOLUME /var/lib/puppet /etc/puppet/modules /etc/puppet/manifests
+VOLUME /etc/puppetlabs/puppet /etc/puppetlabs/code/modules /etc/puppetlabs/code/environments /etc/puppetlabs/puppet/hieradata
 
+RUN groupadd puppet
+RUN useradd -g puppet puppet
 RUN rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
-RUN yum -y install puppet-server hostname tar
+RUN yum -y install which hostname tar puppetserver
+ADD start.sh /start.sh
+ADD puppetserver/auth.conf /etc/puppetlabs/puppetserver/conf.d/auth.conf
+ADD puppetserver/puppetdb.conf /puppetdb.conf
+ADD puppetserver/master.conf /master.conf
+ADD puppetserver/hiera.yaml /hiera.yaml
 
-CMD puppet master --verbose --no-daemonize
+ENV PATH /opt/puppetlabs/bin/:$PATH
+CMD /start.sh
 
 EXPOSE 8140
---
+++ start.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Heavily copied from vpetersson/docker-puppetmaster
+# Thank you :)
+#
+
+function initiate_instance {
+  echo "Starting node initiation..."
+
+  # Fire up regular Puppet master to generate
+  # certificates and folder structure.
+  # This shouldn't take more than five seconds.
+  echo "Starting Puppet to generate certificates..."
+  timeout 5 puppet master --no-daemonize
+
+  echo "Node initation completed..."
+}
+
+# Assume that this is a new instance if
+# no SSL file for the hostname exist.
+if [ ! -f /etc/puppetlabs/puppet/ssl/certs/$(hostname).pem ]; then
+  initiate_instance
+fi
+
+cat - > /etc/puppetlabs/puppet/puppet.conf <<EOF
+[main]
+certname = $(hostname)
+server = $(hostname)
+masterport = 8149
+environment = production
+runinterval = 1h
+EOF
+
+if [ -f /master.conf ]; then
+	cat /master.conf >> /etc/puppetlabs/puppet/puppet.conf
+fi
+
+for CONF in puppetdb.conf hiera.yaml; do
+	if [ -f /$CONF ]; then
+		cp /$CONF /etc/puppetlabs/puppet/
+	fi
+done
+
+if [ ! -d /etc/puppetlabs/code/environments/production ]; then
+	mkdir -p /etc/puppetlabs/code/environments/production
+	chown puppet:puppet /etc/puppetlabs/code/environments
+fi
+
+/opt/puppetlabs/bin/puppetserver foreground
