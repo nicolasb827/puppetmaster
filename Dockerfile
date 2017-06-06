0c7203ce55efc8b2d4c78d9b9f1307c8f59f64f7
Nicolas <nicolas@belan.org>
2017-06-06 15:38:49 +0200

    add labels add autosign capability add environment add verbose startup add possible http_proxy

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,53 @@
 FROM centos:7
 MAINTAINER nicolas.belan@gmail.com
+LABEL network.b2.version="1.0.0"
+LABEL vendor="B2 Network"
+LABEL network.b2.release-date="2017-06-01"
+LABEL network.b2.version.is-production="0"
 
-VOLUME /etc/puppetlabs/puppet /etc/puppetlabs/code/modules /etc/puppetlabs/code/environments /etc/puppetlabs/hieradata 
+# set to 1 to autosign client cert (should be set in dev/testing)
+ARG AUTOSIGN
+# set to not empty string to restrict autosign to a single domain
+ARG AUTOSIGN_DOMAIN
 
+# precise env used
+ARG ENVIRONMENT
+ARG http_proxy
+
+# VOLUMES
+VOLUME /etc/puppetlabs/hieradata /etc/puppetlabs/code/environments/production /etc/puppetlabs/code/environments/dev /etc/puppetlabs/code/modules
+
+RUN if [ "" != "$http_proxy" ]; then echo -e "proxy=http://$http_proxy\n" | tee -a /etc/yum.conf; fi
 RUN groupadd puppet
 RUN useradd -g puppet puppet
 RUN rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
-RUN yum -y install which hostname tar puppetserver
-ADD start.sh /start.sh
-ADD puppetserver/auth.conf /etc/puppetlabs/puppetserver/conf.d/auth.conf
-ADD puppetserver/master.conf /master.conf
-ADD puppetserver/hiera.yaml /hiera.yaml
-ADD puppetserver/sysconfig.puppetserver  /sysconfig.puppetserver
+RUN yum -y install which hostname tar puppetserver rsync git
+RUN mkdir /repo
+RUN mkdir -p /etc/puppetlabs/code/environments
+RUN mkdir -p /etc/puppetlabs/code/modules
+RUN mkdir -p /etc/puppetlabs/hieradata
 
+#WORKDIR /etc/puppetlabs/code
+#RUN git clone https://nbelan@bitbucket.org/nbelan/puppetv4_env.git production
+#RUN git clone https://nbelan@bitbucket.org/nbelan/puppetv4_env.git -b dev dev
+
+COPY start.sh /start.sh
+COPY puppetserver/auth.conf /etc/puppetlabs/puppetserver/conf.d/auth.conf
+COPY puppetserver/master.conf /master.conf
+COPY puppetserver/hiera.yaml /hiera.yaml
+COPY puppetserver/sysconfig.puppetserver  /sysconfig.puppetserver
+COPY puppetserver/production/ /repo/
+
+
+# allow puppet path in PATH
 ENV PATH /opt/puppetlabs/bin/:$PATH
-CMD /start.sh
+ENV AUTOSIGN ${AUTOSIGN}
+ENV AUTOSIGN_DOMAIN ${AUTOSIGN_DOMAIN}
+ENV ENVIRONMENT ${ENVIRONMENT}
+
+
+
+# ENTRYPOINT
+CMD /start.sh $AUTOSIGN $ENVIRONMENT $AUTOSIGN_DOMAIN
 
 EXPOSE 8140
--- start.sh
+++ start.sh
@@ -4,56 +4,103 @@
 # Thank you :)
 #
 
+AUTOSIGN=$1
+ENVIRONMENT=$2
+AUTOSIGN_DOMAIN=$3
+
+if [ "1" == "$AUTOSIGN" ]; then
+	AUTOSIGN=1
+	echo "[master] I am doing autosign"
+else
+	echo "[master] I am not doing autosign"
+	AUTOSIGN=0
+fi
+
+if [ "" == "$ENVIRONMENT" ]; then
+	ENVIRONMENT="production"
+fi
+echo "[master] Running with ENV=$ENVIRONMENT"
+
 function initiate_instance {
-  echo "Starting node initiation..."
+  echo "[master] Starting node initiation..."
+
+  rm -rf /etc/puppetlabs/puppet/ssl
+  mkdir -p /etc/puppetlabs/puppet/ssl
+  chown puppet:puppet /etc/puppetlabs/puppet/ssl
 
   # Fire up regular Puppet master to generate
   # certificates and folder structure.
   # This shouldn't take more than five seconds.
-  echo "Starting Puppet to generate certificates..."
-  timeout 5 puppet master --no-daemonize
+  echo "[master] Starting Puppet to generate certificates... (10 sec)"
+  timeout 10 puppet master --no-daemonize
 
-  echo "Node initation completed..."
+  echo "[master] Node initation completed..."
 }
 
 # Assume that this is a new instance if
 # no SSL file for the hostname exist.
-if [ ! -f /etc/puppetlabs/puppet/ssl/certs/$(hostname).pem ]; then
+if [ ! -f /etc/puppetlabs/puppet/ssl/certs/puppetmaster.pem ]; then
   initiate_instance
 fi
 
+echo "[master] generating master puppet.conf"
 cat - > /etc/puppetlabs/puppet/puppet.conf <<EOF
 [main]
-certname = $(hostname)
-server = $(hostname)
-masterport = 8149
-environment = production
+certname = puppetmaster
+server = puppetmaster
+masterport = 8140
+environment = $ENVIRONMENT
 runinterval = 1h
 basemodulepath = /etc/puppetlabs/code/modules
 EOF
 
 if [ -f /master.conf ]; then
+	echo "[master] adding master.conf to puppet.conf"
 	cat /master.conf >> /etc/puppetlabs/puppet/puppet.conf
+fi
+if [ "1" == "$AUTOSIGN" ]; then
+	if [ "" == "$AUTOSIGN_DOMAIN" ]; then
+		# append autosign to master conf
+		echo "autosign = true" >> /etc/puppetlabs/puppet/puppet.conf
+	else
+		echo "Should add whitelist to Puppet Conf ... WIP"
+		echo "autosign = true" >> /etc/puppetlabs/puppet/puppet.conf
+	fi
 fi
 
 for CONF in puppetdb.conf hiera.yaml; do
 	if [ -f /$CONF ]; then
+		echo "[master] copy $CONF to /etc/puppetlabs/puppet/"
 		cp /$CONF /etc/puppetlabs/puppet/
 	fi
 done
 
 if [ -f /sysconfig.puppetserver ]; then
+	echo "[master] copy sysconfig(puppet)"
 	cp /sysconfig.puppetserver /etc/sysconfig/puppetserver
 fi
 
-if [ ! -d /etc/puppetlabs/code/environments/production ]; then
-	mkdir -p /etc/puppetlabs/code/environments/production
-	chown puppet:puppet /etc/puppetlabs/code/environments
+for D in production dev; do
+	if [ ! -d /etc/puppetlabs/code/environments/$D ]; then
+		echo "[master] creating directory /etc/puppetlabs/code/environments/$D"
+		mkdir -p /etc/puppetlabs/code/environments/$D
+	fi
+done
+
+if [ ! -d /etc/puppetlabs/code/environments/$ENVIRONMENT ]; then
+	echo "[master] finally, creating directory /etc/puppetlabs/code/environments/$ENVIRONMENT"
+	mkdir -p /etc/puppetlabs/code/environments/$ENVIRONMENT
 fi
+echo "[master] copying root repository into production"
+rsync -avH /repo/ /etc/puppetlabs/code/environments/production/
+
+echo "[master] changing owner on environment files"
+chown -R puppet:puppet /etc/puppetlabs/code/environments
 
 if [ ! -d /etc/puppetlabs/puppetserver/conf.d ]; then
 	mkdir -p /etc/puppetlabs/puppetserver/conf.d
 	chown puppet:puppet /etc/puppetlabs/puppetserver/conf.d
 fi
 
+echo "[master] starting master ..."
 /opt/puppetlabs/bin/puppetserver foreground
